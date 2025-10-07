12970965f5d8111ecd0041794dc20a81fe4530cb
DANIEL ROGER <72160504+danieldr22@users.noreply.github.com>
2025-10-07 19:29:01 -0300

    projeto.py

--- projeto.py
+++ projeto.py
@@ -9,4 +9,19 @@ except AttributeError:
     exit()
 model = genai.GenerativeModel('gemini-1.5-flash')
 
+for pasta in [pasta_moto_azul, pasta_moto_vermelha, pasta_nao_moto]:
+    if not os.path.exists(pasta):
+        os.makedirs(pasta)
+if not os.path.exists(pasta_origem):
+    print(f"ERRO: A pasta '{pasta_origem}' não existe . Crie uma e adicione suas imagens.")
+    exit()
+
+for nome_arquivo in os.listdir(pasta_origem):
+    caminho_arquivo = os.path.join(pasta_origem, nome_arquivo)
+
+    if os.path.isfile(caminho_arquivo):
+        print(f"Analisando '{nome_arquivo}'...")
+        try:
+            imagem = genai.upload_file(caminho_arquivo)
+
 
